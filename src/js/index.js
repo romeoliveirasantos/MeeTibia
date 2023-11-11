14128e5eba6994b05f9943f9605818b4bd853ea6
romeoliveirasantos <rjob.oliveira@gmail.com>
2023-11-11 17:04:18 -0300

    Feat: Javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 const buttons = document.querySelectorAll(".button");
-
 const characters = document.querySelectorAll(".character");
+const buttonsSpells = document.querySelectorAll(".button-spell");
+const spellDesc = document.querySelectorAll(".descText");
+const spellDescContainer = document.querySelectorAll(".spellDescription");
+
+
 
 //forEach para capturar eventos dos botões
 buttons.forEach((button, indice) =>{
@@ -8,10 +12,11 @@ buttons.forEach((button, indice) =>{
 
     desselectButton();
     desslectCharacter();
+    
 
     button.classList.add("selected");
     characters[indice].classList.add("selected");
-
+    
   });
 });
 
@@ -27,3 +32,28 @@ function desslectCharacter(){
     character.classList.remove("selected");
   });
 }
+
+
+//forEach para capturar eventos dos botões de spells
+buttonsSpells.forEach((buttonSpell, indice) =>{
+  buttonSpell.addEventListener("click", ()=>{
+
+    desselectButtonSpells();
+    desselectContainerDesc();
+    buttonSpell.classList.add("selected");
+  });
+});
+
+
+function desselectButtonSpells(){
+  buttonsSpells.forEach((buttonS)=>{
+    buttonS.classList.remove("selected");
+  });
+}
+
+
+
+
+// // <div class="spellDescription">
+// <p class="descText">3</p>
+// </div>
